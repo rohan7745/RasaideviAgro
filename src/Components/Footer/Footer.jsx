851f4f12b6f2deb7f7e6a713eac7ad43e370cb5b
rohan7745 <rohanwaghmare735@gmail.com>
2025-01-23 10:29:37 +0530

    final

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -19,6 +19,7 @@ const Footer = () => {
             <hr className='logo_hr' />
             <ul className="footer_links">
                 <li onClick={goToTop}><Link to='/'>Home</Link></li>
+                {/*<li onClick={goToTop}><Link to='/products'>Products</Link></li>*/}
                 <li onClick={goToTop}><Link to='/about'>About</Link></li>
                 <li onClick={goToTop}><Link to='/gallery'>Gallery</Link></li>
                 <li onClick={goToTop}><Link to='/contact'>Contact</Link></li>
